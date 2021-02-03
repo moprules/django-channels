7cdc3cd1d8b85785deec095d26b2f11e723574b0
ogurechik <rav-navini-gego-cutropal@yandex.ru>
2021-02-03 18:25:13 +0300

    Закончили часть 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Указываем асинхронное приложение для запуска кода
+# Channels
+ASGI_APPLICATION = 'mysite.routing.application'
